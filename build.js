bbb3c0442c4774520497903e877625c2879fe963
c8idu <0711kps@gmail.com>
2018-12-18 20:30:09 +0800

    build script renew

--- build.js
+++ build.js
@@ -1,7 +1,18 @@
-const { copyFile, writeFile, readFile, readdir } = require('fs')
+const { copyFile, writeFile, readFile, readFileSync, readdir, readdirSync } = require('fs')
 const { extname, basename } = require('path')
-const { pathSplit, compileLS, minifyJS, compilePug, compileStylus, minifyCS, minifyJSON } = require('./k-lib.js')
+const { compileLS, minifyJS, compilePug, compileStylus, minifyCSS, minifyJSON } = require('./k-lib.js')
 const browsers = ['firefox', 'chrome']
+
+let images = readdirSync('src/images').map(img => {
+  return { source: `src/images/${img}`, filename: img }
+})
+let htmls = readdirSync('src/pug').map(pug => {
+  return { filename: pug.replace('.pug', '.html'), content: compilePug(`src/pug/${pug}`) }
+})
+let csses = readdirSync('src/stylus').map(styl => {
+  let stylContent = readFileSync(`src/stylus/${styl}`,{ encoding: 'utf-8' })
+  return { filename: styl.replace('.styl', '.css'), content: minifyCSS(compileStylus(stylContent)).css }
+})
 
 browsers.forEach(browser => {
   readFile(`src/${browser}/manifest.json`, { encoding: 'utf-8' }, (err, data) => {
@@ -11,6 +22,24 @@ browsers.forEach(browser => {
       console.log(`${browser} manifest file copied`)
     })
   })
+  images.forEach(img => {
+    copyFile(img.source, `build/${browser}/images/${img.filename}`, err => {
+      if(err) throw err
+    })
+  })
+  console.log('images copied!')
+  htmls.forEach(html => {
+    writeFile(`build/${browser}/html/${html.filename}`, html.content, err => {
+      if(err) throw err
+    })
+  })
+  console.log('html compiled!')
+  csses.forEach(css => {
+    writeFile(`build/${browser}/css/${css.filename}`, css.content, err => {
+      if(err) throw err
+    })
+  })
+  console.log('css compiled!')
   readdir(`src/${browser}/ls`, (err, lsFiles) => {
     lsFiles.forEach(ls => {
       let lsPath = `src/${browser}/ls/${ls}`
