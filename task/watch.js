050e736776c171351848ef46deba83dd83cda430
c8idu <0711kps@gmail.com>
2019-01-22 13:29:54 +0800

    replace fs.watch with chokidar for problems in different os

--- task/watch.js
+++ task/watch.js
@@ -1,17 +1,17 @@
-const { watch } = require('fs')
+const chokidar = require('chokidar')
 const { extname } = require('path')
 const { handlers } = require('./k-lib.js')
 
-watch('src', { recursive: true }, (event, filePath) => {
+chokidar.watch('src').on('change', (filePath, event) => {
   let ext = extname(filePath)
   let preventDuplicateEvent = null
   if (!filePath.includes('#') && ext) {
     if (typeof preventDuplicateEvent !== 'undefined') clearTimeout(preventDuplicateEvent)
     preventDuplicateEvent = setTimeout(() => {
-      let formatedFilePath = `src/${filePath.replace(/\\/g, '/')}`
+      let formatedFilePath = filePath.replace(/\\/g, '/')
       let handlerName = formatedFilePath.split('/')[2]
       handlers[handlerName](formatedFilePath)
-      console.log(`the change spotted: ${formatedFilePath}`)
+      console.log(`the change spotted and handled: ${formatedFilePath}`)
     }, 200)
   }
 })
